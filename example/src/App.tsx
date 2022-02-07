6f400987f93ce5fb3adeb6030a47a584fc02409d
Yaman KATBY <m.yaman.katby@gmail.com>
2022-02-07 14:10:22 +0300

    🎨 Formatting code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { odd, Selector, even } from 'react-native-flex-layout';
 
-export default function() {
-  return (
-    <Selector style={even({ backgroundColor: 'gray' })}>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-      <Text>Hello</Text>
-    </Selector>
-  );
+export default function App() {
+  return <Text>Hello, world!</Text>;
 }
