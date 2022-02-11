7a8b1268abcaf8ab4cee5d2d0741f09ad5060c31
Yaman KATBY <m.yaman.katby@gmail.com>
2022-02-11 15:46:33 +0300

    ✨ Add sitemap.xml

---
+++ docs/next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://react-native-flex-layout.js.org/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
