a20ac30cbfecd542bd649860180baca630018f4b
Yaman KATBY <m.yaman.katby@gmail.com>
2022-02-11 15:57:09 +0300

    ✨ Add GTAG script

---
+++ docs/pages/_document.js
@@ -0,0 +1,24 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-PQTP3P9QQW"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-PQTP3P9QQW");`,
+          }}
+        />
+      </Head>
+      <body>
+      <Main />
+      <NextScript />
+      </body>
+    </Html>
+  );
+}
