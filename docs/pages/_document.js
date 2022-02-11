69aa073ac82d653b16c09d55b7bd85ac11da3621
Yaman KATBY <m.yaman.katby@gmail.com>
2022-02-11 16:59:59 +0300

    🔎 Work on the docs SEO

--- docs/pages/_document.js
+++ docs/pages/_document.js
@@ -14,6 +14,21 @@ export default function Document() {
             __html: `function gtag(){dataLayer.push(arguments)}window.dataLayer=window.dataLayer||[],gtag("js",new Date),gtag("config","G-PQTP3P9QQW");`
           }}
         />
+        
+        <meta name="title" content="React Native Flex Layout" />
+        <meta name="description" content="Layout primitives for React Native." />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://react-native-flex-layout.js.org/" />
+        <meta property="og:title" content="React Native Flex Layout" />
+        <meta property="og:description" content="Layout primitives for React Native." />
+        <meta property="og:image" content="/cover.png" />
+
+        <meta property="twitter:card" content="/cover.png" />
+        <meta property="twitter:url" content="https://react-native-flex-layout.js.org/" />
+        <meta property="twitter:title" content="React Native Flex Layout" />
+        <meta property="twitter:description" content="Layout primitives for React Native." />
+        <meta property="twitter:image" content="/cover.png" />
       </Head>
       <body>
       <Main />
