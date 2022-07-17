7aa70420e51bc5df1a2d60dfcf9aa11b7347487d
Yaman KATBY <m.yaman.katby@gmail.com>
2022-07-17 18:01:22 +0300

    docs: update home page logo

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -12,7 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img src="/img/logo.svg" alt="logo" />
+        <img src="/img/white-logo.svg" alt="logo" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
